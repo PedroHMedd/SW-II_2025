0be9f44f9b4d7e708cf4774c7aa9e4ebf8d798f2
Pedro Medeiros <161343084+PedroHMedd@users.noreply.github.com>
2025-02-11 11:10:16 -0300

    Add files via upload

---
+++ aula01/calculamedia.php
@@ -0,0 +1,12 @@
+<?php $nota1 = 7;
+$nota2 = 9;
+$nota3 = 10;
+ $media = ($nota1 + $nota2 + $nota3)/3;
+ echo ("De acordo com suas últimas 3 notas sua média é: $media"); 
+ if ($media >= 7)
+ echo "Você foi aprovado!";
+
+ else 
+ echo "Você foi reprovado!";
+ 
+ ?>
---
+++ aula01/pagina.php
@@ -0,0 +1,1 @@
+<?php echo "Olá, Professor Anderson, tudo bem? Estou treinando PHP!"; ?>
