0b75bb7117d3704afa90f9156c37d2bef2d4e93c
Pedro Medeiros <161343084+PedroHMedd@users.noreply.github.com>
2025-02-12 20:41:51 -0300

    Add files via upload

--- aula01/calculamedia.php
+++ aula01/calculamedia.php
@@ -1,12 +1,15 @@
-<?php $nota1 = 7;
+<?php
+$nota1 = 7;
 $nota2 = 9;
 $nota3 = 10;
- $media = ($nota1 + $nota2 + $nota3)/3;
- echo ("De acordo com suas últimas 3 notas sua média é: $media"); 
- if ($media >= 7)
- echo "Você foi aprovado!";
 
- else 
- echo "Você foi reprovado!";
- 
- ?>
+$media = ($nota1 + $nota2 + $nota3) / 3;
+
+echo "De acordo com suas últimas 3 notas, sua média é: " . round($media, 2) . "<br>";
+
+if ($media >= 7) {
+    echo "Você foi aprovado!";
+} else {
+    echo "Você foi reprovado!";
+}
+?>
