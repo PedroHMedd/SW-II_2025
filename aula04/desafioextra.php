ff39582df9e39c53a4e14a77b66d87ed0674195e
Pedro Medeiros <161343084+PedroHMedd@users.noreply.github.com>
2025-03-24 23:37:52 -0300

    Update desafioextra.php

--- aula04/desafioextra.php
+++ aula04/desafioextra.php
@@ -1,5 +1,5 @@
 <?php
-    $boletim = array("Wesley" => "8", "Pedro" => "10", "Lucas" => "9", "Tiago" => "7");
+    $boletim = array("Kasparov" => "8", "Karpov" => "10", "Tal" => "9", "Boris" => "7");
     foreach ($boletim as $key => $value) {
         sort($value);
         $maior = $value[count($boletim) - 1];
