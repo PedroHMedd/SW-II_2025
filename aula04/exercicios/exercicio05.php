e9832ae288398462e6cefedbd36b4e8cb26dabbf
Pedro Medeiros <161343084+PedroHMedd@users.noreply.github.com>
2025-04-10 21:17:46 -0300

    Delete aula04/exercicios directory

--- aula04/exercicios/exercicio05.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $boletim = array("Paulo" => "9", "Pedro" => "8", "Iago" => "8", "Tizao" => "9");
-    $soma = 0;
-    foreach ($boletim as $key => $value) {
-        $soma += $value;
-    }
-    $media = $soma / count($boletim);
-    echo "A média é " . $media;
-?>
